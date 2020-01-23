20390c3caad27b42930530c6dd6797aec6a03867
Mathieu Kruk <mathieukruk@gmail.com>
2020-01-23 16:28:12 +0100

    bootstrap footer col

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (  
             <Container fluid={true} className={'mt-2'}>
                     <Row>
-                        <Col md={6} className={'componentFooter d-flex justify-content-around align-items-center'}>
+                        <Col xs={12} md={6} className={'componentFooter d-flex justify-content-around align-items-center'}>
                             <small>Follow us:</small>
                             
                             <a link href="https://github.com/MathieuKruk" target="_blank" rel="noopener noreferrer"><i className={"footerIcon fa fa-lg fa-github"} aria-hidden="true"></i></a>
@@ -18,7 +18,7 @@ const Footer = () => {
                             <a link href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><i className={"footerIcon fa fa-lg fa-instagram"} aria-hidden="true"></i></a>
                         </Col>
                 
-                        <Col md={6} className={'componentFooter d-flex justify-content-center align-items-center'}>
+                        <Col xs={12} md={6} className={'componentFooter d-flex justify-content-center align-items-center'}>
                             <small>&copy; Copyright 2019, BeCode: Kiza Motoko, Mathieu Kruk</small>
                         </Col>
                 
